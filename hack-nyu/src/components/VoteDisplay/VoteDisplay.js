a2d032f0609c80be98467f69936adb6f302aae05
willmonahan <willmonahan13@gmail.com>
2019-02-16 12:52:52 -0500

    vote display reacts to loading of events array (no pun intended)

--- hack-nyu/src/components/VoteDisplay/VoteDisplay.js
+++ hack-nyu/src/components/VoteDisplay/VoteDisplay.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
-
-
 // Vote Display expects a prop called EventName, which displays the event at the top of the stack
 function VoteDisplay(props) {
-    const name = props.event.name;
-    return <h1>{name}</h1>;
+  return <h1>{props.event ? props.event.name : 'No event...'}</h1>;
 }
-
 
 export default VoteDisplay;
