74981310fdebc942e4dc9a67d93c8ee4d6f1e928
willmonahan <willmonahan13@gmail.com>
2019-02-15 23:58:05 -0500

    setup mongoose schema, models, and local config

---
+++ backend/db.js
@@ -0,0 +1,72 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema.Types;
+
+let db;
+let user;
+let pass;
+
+if (process.env.NODE_ENV === 'development') {
+  db = 'mongodb://localhost/hackNYU';
+} else {
+  user = process.env.DB_USER;
+  pass = process.env.DB_PASS;
+  db = ''; // FILL THIS IN LATER
+}
+
+const communitySchema = new mongoose.Schema({
+  name: String,
+  users: [
+    {
+      type: ObjectId,
+      ref: 'User',
+    },
+  ],
+  events: [
+    {
+      type: ObjectId,
+      ref: 'Event',
+    },
+  ],
+});
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  community: {
+    type: ObjectId,
+    ref: 'Community',
+  },
+  events: {
+    type: ObjectId,
+    ref: 'Event',
+  },
+});
+
+const eventSchema = new mongoose.Schema({
+  name: String,
+  desc: String,
+  creator: {
+    type: ObjectId,
+    ref: 'User',
+  },
+  yesList: [
+    {
+      type: ObjectId,
+      ref: 'User',
+    },
+  ],
+  noList: [
+    {
+      type: ObjectId,
+      ref: 'User',
+    },
+  ],
+});
+
+mongoose.model('Community', communitySchema);
+mongoose.model('User', userSchema);
+mongoose.model('Event', eventSchema);
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+});
