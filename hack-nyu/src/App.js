239a919e48997eda077b958705df2eda12656566
ChallaHalla <sc5455@nyu.edu>
2019-02-16 14:04:35 -0500

    Router working

--- hack-nyu/src/App.js
+++ hack-nyu/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
+import { Router, Route, Switch } from 'react-router';
 import VoteView from './containers/VoteView/VoteView';
+import { createBrowserHistory } from "history";
+
+
 
 class App extends Component {
   render() {
+    const hist = createBrowserHistory();
+    const App = () => (
+    <div>
+      <Switch>
+        <Route exact path='/vote' component={VoteView}/>
+      </Switch>
+    </div>
+  )
     return (
-      <div>
-        <VoteView />
-      </div>
+      <Router history={hist}>
+      <App/>
+      </Router>
     );
   }
 }
