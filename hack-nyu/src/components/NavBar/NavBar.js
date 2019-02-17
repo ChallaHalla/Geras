8dc9ed32413290d43ac52903d21ab3266a9908de
willmonahan <willmonahan13@gmail.com>
2019-02-16 23:50:38 -0500

    cleanup time

--- hack-nyu/src/components/NavBar/NavBar.js
+++ hack-nyu/src/components/NavBar/NavBar.js
@@ -6,7 +6,7 @@ function NavBar(currentPage) {
     <div className='navBar'>
       <a href='/events'>Events Page</a>
       <a href='/vote'> Vote Page</a>
-      <img src='' alt='Logo' />
+      <img src='' alt='Logo' style={{ display: 'none' }} />
     </div>
   );
 }
