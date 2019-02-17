bec789529616b923b46395d1fcc3ee6022973c2b
willmonahan <willmonahan13@gmail.com>
2019-02-17 10:11:31 -0500

    small cleaning refactor

--- hack-nyu/src/components/NameList/NameList.js
+++ hack-nyu/src/components/NameList/NameList.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import SignUp from '../SignUp/SignUp';
-import './NameList.css'
+import './NameList.css';
 
-class NameList extends Component{
-    render(){
-        const userName = {};
-        // const names =["User1 ","User2 ","User3 ","User4 "]
-        const names = this.props.names || [];
-        return (
-            <div>
-            {names.map((n)=>{
-              return <button className="button is-large is-fullwidth nameButton"name="username" value={n} onClick={(e)=>{
+class NameList extends Component {
+  render() {
+    const names = this.props.names || [];
+    return (
+      <div>
+        {names.map((n) => {
+          return (
+            <button
+              className='button is-large is-fullwidth nameButton'
+              name='username'
+              value={n}
+              onClick={(e) => {
                 this.props.progressStage();
                 this.props.handleChange(e);
-              }}>{n}</button>;
-            })}
-            </div>
-        )
-    }
+              }}
+            >
+              {n}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default NameList;
